6d64d5a8b6c1a4390a4ae1831e9a05776f8fdebf
AaronGoodrum <ag505181@gmail.com>
2018-08-13 19:13:43 -0700

    week2

--- 1-HTML-CSS-JavaScript/README.md
+++ 1-HTML-CSS-JavaScript/README.md
@@ -1,1 +1,3 @@
 # Nucamp-Folder
+## Week 2
+  In the second week of the course you will begin to learn JavaScript. This includes the fundamental components of most programming languages such as variables, arrays, making decisions, loops, events and functions, as well as the generation of random numbers.
---
+++ 1-HTML-CSS-JavaScript/js_guessing_game.js
@@ -0,0 +1,44 @@
+let target;
+let guess_input_text;
+let guess_input;
+let finished = false;
+let guesses = 0;
+
+function do_game(){
+	let random_number = Math.random() * 100;
+	let random_number_integer = Math.floor(random_number);
+	target = random_number_integer + 1;
+
+	while (!finished) {
+		guess_input_text = prompt("I am thinking of a number " + 
+		"in the range 1 to 100.\n\n" +
+		"What is the number? ");
+		guess_input = parseInt(guess_input_text);
+		guesses += 1;
+		finished = check_guess();
+	}
+}
+
+function check_guess() {
+	if (isNaN(guess_input)) {
+		alert("You have not entered a number.\n\n" +
+		"Please enter a number in the range 1 to 100.");
+		return false;
+	}
+	if ((guess_input < 1) || (guess_input > 100)) {
+		alert("Please enter an integer number in the range 1 to 100.");
+		return false;
+	}
+	if (guess_input > target) {
+		alert("Your number is too large!");
+		return false;
+	}
+	if (guess_input < target) {
+		alert("Your number is too small!");
+		return false;
+	}
+	alert("You got it! The number was " + target +
+	".\n\nIt took you " + guesses +
+	" guesses to get the number!");
+	return true;
+}
